d73f9973112c61bd7badd582a4f71eab91119717
Cédric Righi <cedric.righi@gmail.com>
2024-10-29 00:07:23 +0100

    Update MainScreen.tsx

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -3,7 +3,7 @@ function MainScreen() {
 		<div className="card">
 			<p>
 				Êtes-vous prêts à tester vos connaissances cinématographiques ? Trouvez
-				de quels films sont issus ces répliques.
+				de quels films sont issues ces répliques.
 			</p>
 			<button type="button">Commencer le Quiz</button>
 		</div>
