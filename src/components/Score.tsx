f1cd2e07ef4145586b6fafea5642f4839b24cf86
Gamberge <cedric.righi@gmail.com>
2024-10-29 00:02:03 +0100

    adding logic to nav through questions

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -1,5 +1,9 @@
-export default function Score() {
+interface scoreProps {
+  score: number;
+}
+
+export default function Score({ score }: scoreProps) {
   return (
-    <div className="score">Score</div>
+    <div className="score">Score : {score}/10</div>
   )
 }
